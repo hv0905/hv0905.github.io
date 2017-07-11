2cba0f6f15e8fe8494a4a075ccf1f981c22845f6
HIM <hv0905@outlook.com>
2017-07-11 21:49:31 +0800

    1

--- viewing.js
+++ viewing.js
@@ -1,11 +1,12 @@
 // JavaScript Document
 
-	function tl_mouseOver(elements){
-		"use strict";
-		elements.className = 'head_item selecting';
-	}
+function tl_mouseOver(elements){
+	"use strict";
+	elements.className = 'head_item selecting';
+}
 	
-	function tl_mouseOut(elements){
-		"use strict";
-		elements.className = 'head_item';
-	}
+function tl_mouseOut(elements){
+	"use strict";
+	elements.className = 'head_item';
+}
+
